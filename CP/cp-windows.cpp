8f8cb5f29ba4466a4306e8ebd3167bc3c55676b6
Sayeed Joy <51468562+sayeedjoy@users.noreply.github.com>
2024-11-16 19:42:01 +0600

    Create cp-windows.cpp

---
+++ CP/cp-windows.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> //comment out while not on Mac
+/*#include <iostream>
+#include<algorithm>
+#include<unordered_map>
+#include<vector>
+#include<unordered_set>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>*/
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+
+int main() {
+    fast;
+    
+}
