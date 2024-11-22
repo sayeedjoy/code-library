f3f5fdcd5d569a2731ce4f30a1e6f7d2761ffd12
Sayeed Joy <51468562+sayeedjoy@users.noreply.github.com>
2024-11-22 20:58:44 +0600

    Update cp-template.cpp
    Update header files

--- CP/cp-template.cpp
+++ CP/cp-template.cpp
@@ -1,14 +1,31 @@
-//#include<bits/stdc++.h> //comment out while not on Mac
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cstdint>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
 #include <iostream>
-#include<algorithm>
-#include<unordered_map>
-#include<vector>
-#include<unordered_set>
-#include<set>
-#include<map>
-#include<queue>
-#include<stack>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
+
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define endl "\n"
 #define ll long long
@@ -16,5 +33,4 @@ using namespace std;
 
 int main() {
     fast;
-    
 }
